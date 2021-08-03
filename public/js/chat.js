1b1b0bcf1c537b055a999eb6e77a22edac3d6f7c
lee <o5046821854@gmail.com>
2021-08-03 23:55:05 +0900

    static web using form

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,7 +1,30 @@
 const socket = io() //얘가 받음
 
-socket.on('countUpdated', (count)=>{  //서버에서 1param으로 보낸 것을 받음, 2nd: 서버의 2nd param
-    console.log("the count has been updated" + count)
+// socket.on('countUpdated', (count)=>{  //서버에서 1param으로 보낸 것을 받음, 2nd: 서버의 2nd param
+//     console.log("the count has been updated" + count)
+// })
+
+
+//입력 시 버튼, 폼들을 변환시키기 위한 변수들
+const $messageForm = document.querySelector("#mess")
+const $messageFormInput = document.querySelector("input")
+const $messageFormButton = document.querySelector("button")
+const $setlocationButton = document.querySelector("#send-location")
+const $locationMessageTemplate = document.querySelector("#location-maeeage-template")
+
+
+const $messages = document.querySelector('#messages')
+
+const messageTemplate = document.querySelector("#message-template").innerHTML
+
+socket.on('message',(Message)=>{
+    console.log(Message)
+    const html = Mustache.render(messageTemplate, {
+        message: message.text,
+        createAt : moment(message.createdAt).format("h:mm a") //(momontjs 라이브러리가 제공하는 시간 포맷 지정)
+    })  //2nd param : html에서 끌어다 쓸 값들 {{}} 로 씀
+    console.log("asdsa" + message)
+    $messages.insertAdjacentHTML('beforeend', html)
 })
 
 document.querySelector("#increment").addEventListener('click', ()=>{  //html 컴포넌트에 대해 이벤트 리스너 정의 
@@ -10,3 +33,52 @@ document.querySelector("#increment").addEventListener('click', ()=>{  //html 컴
     socket.emit('increment')  //client -> server
 })
 
+socket.on('locationMessage', (url) =>{
+    console.log(url)
+    const html = Mustache.render($locationMessageTemplate, {
+        url
+    })
+    $messages.insertAdjacentHTML("beforeend",html)
+})
+
+$messageForm.addEventListener('submit', (e)=>{  //html 컴포넌트에 대해 이벤트 리스너 정의 
+    e.preventDefault() //submit은 누르면 페이지가 리프레쉬되는데 이 기능을 막고자
+    
+    $messageFormButton.setAttribute("disabled", "disabled") //submit 누른 순간 버튼 비활성화
+     
+    const message = e.target.elements.message.value   //e.target.elements.message : 이벤트리스너가 감지하는 것 (form_) + elements.'name' => form 중에 name 속성이 'name' 것을 참조 (input 태그를 queryselector로 안 하는 것이 좋을 때)
+    socket.emit("send", message, (error)=>{   //3param : acknowledge가 수신측으로 부터 들어왔을 때 핸들러
+        
+        //ack를 받으면, 버튼 활성화, 인풋에 들어있던 값 지워지고 커서가 들어감
+        $messageFormButton.removeAttribute("disabled") 
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
+        if(error){
+            return console.log(error)
+        }
+        console.log("the message is delivered")
+    }) //client -> server
+})
+
+//document.querySelector('#send-location').addEveneListenr 써도됨
+$setlocationButton.addEventListener('click', ()=>{
+    
+    if(!navigator.geolocation) //지원하지 않을 경우
+    {
+        return alert("geolocation not support")
+    }
+
+    $setlocationButton.getAttribute("disabled", "disabled")
+
+    navigator.geolocation.getCurrentPosition((position)=>{  //getcurrentposition : 비동기함수임 : 람다식으로 적어줌
+        console.log(position)  //navigator.geolocation : js에서 제공하는 사용자의 위치 정보 api
+        socket.emit("sendLocation", {  //emit을 객체로 전달
+            latitude : position.coords.latitude,
+            longitude : position.coords.longitude,
+        }, (msg)=>{  //ack를 수신측에서 받음
+            $setlocationButton.removeAttribute("disabled")
+            console.log(msg)
+        })
+    })
+})
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const path = require('path')
 const http = require('http')
-
+const Filter = require("bad-words")
 const express = require("express")
 const app = express()
 const server  = http.createServer(app)
 const socketio = require('socket.io')
 const io = socketio(server)  //파라미터로 서버가 들어가야함 (그래서 http.createServer 해준 것)
+const {generateMessage} = require('./utils/messages')
 
 const port = process.env.PORT || 3000
 
@@ -17,12 +18,36 @@ let count = 0
 
 io.on('connection',(socket)=>{   //소켓 이벤트리스너, 콜백
     console.log("new websocket connected")
-    socket.emit('countUpdated', count)  //서버에서 클라이언트로 전송, 2nd param으로 전해주는 데이터는 클라이언트 사이드에서 콜백으로 접근 가능
     
-    socket.on('increment', ()=>{  //클라이언트에서 들어오는 정보를 받음
-        count++
-        //socket.emit("countUpdated", count)  //클라이언트로 던져 (얘는 하나의 클라이언트로 던짐)
-        io.emit("countUpdated", count) //얘는 모든 클라이언트로 던짐
+    socket.emit("message", generateMessage("welcome")) //utils의 index.js에 전달됨
+    // socket.emit('countUpdated', count)  //서버에서 클라이언트로 전송, 2nd param으로 전해주는 데이터는 클라이언트 사이드에서 콜백으로 접근 가능
+    
+    // socket.on('increment', ()=>{  //클라이언트에서 들어오는 정보를 받음
+    //     count++
+    //     //socket.emit("countUpdated", count)  //클라이언트로 던져 (얘는 하나의 클라이언트로 던짐)
+    //     io.emit("countUpdated", count) //얘는 모든 클라이언트로 던짐 (count가 다른 클라이언트에게도 전달됨)
+    // })
+
+    socket.broadcast.emit("message", "new member join") //타겟 클라이언트를 제외한 모든 클라이언트로 던짐
+
+    socket.on('send', (msg, callback)=>{ //클라이언트에서 들어오는 정보를 받음
+        const filter = new Filter()
+        console.log(msg)
+        if(filter.isProfane(msg)){
+            return callback("profanity not allowed")  //욕설이 들어오면 callback function
+        }
+        
+        io.emit("message", msg) //모든 클라이언트로 던짐 (msg가 다른 클라이언트에게도 전달됨)
+        callback()  //송신 측에 acknowledge 전달
+    })
+
+    socket.on('disconnect', ()=>{   //disconnect 시에는 io.on이 아니라 socket.on으로 처리
+        io.emit("message", "user has left")  //disconnect에는 io로 줘도 타겟 클라이언트 제외한 모든 클라이언트로 다 전달됨 (걔는 나갔으니까)
+    })
+
+    socket.on('sendLocation', (coords, callback)=>{
+        io.emit('locationMessage', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)  //전달할 떄 구글 맵 url에다가 위,경도 띄워서 전달해주기 (링크타고 구글 맵 이동할 수 있음)
+        callback("well delivered") //ack 전송
     })
 })
 
